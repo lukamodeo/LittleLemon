7f330c5beaedb04b5267c4198a5c54fd34c10202
Luca <lukamodeo@tiscali.it>
2026-09-05 19:15:55 +0200

    create Menu and Booking models

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Menu
+from .models import Booking
 
 # Register your models here.
+
+admin.site.register([
+    Menu,
+    Booking,
+])
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(default=1)
+    booking_date = models.DateField()
+
+    # Explicitly declare the default manager
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(default=0)
+
+    # Explicitly declare the default manager
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.title
+
+
